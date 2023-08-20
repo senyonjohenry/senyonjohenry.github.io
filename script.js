33ab7c02f27d1f37c13e8fb5352f21296d2a63de
SENYONJO HENRY <senyonjohenry1@gmail.com>
2023-08-20 16:54:08 +0300

    initial commit for the app website

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scroll effect for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
